47e0f62aff3f1331074afe5a2825ccdb24bd14de
Abdulaziz <77390086+DankCoffee@users.noreply.github.com>
2026-03-24 18:34:11 +0400

    Update debugprint.c

--- launcher/source/debugprint.c
+++ launcher/source/debugprint.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <sys/iosupport.h>
 #include <ogc/machine/processor.h>
-#include "files.h"
 
 #include <string.h>
 #include <unistd.h>
@@ -11,13 +10,16 @@
 extern const devoptab_t *devoptab_list[];
 
 static int socket = -1;
+static int connection_failed = 0;
 
 static void InitializeNetwork(const char *ip_str, const int port)
 {
 	int init;
 	if (socket>=0)
 		return;
-	
+
+	connection_failed = 0;
+
 	// Retry net_init (can return -EAGAIN initially)
 	int retries = 100;
 	while (retries-- > 0 && (init = net_init()) < 0) {
@@ -28,15 +30,17 @@ static void InitializeNetwork(const char *ip_str, const int port)
 		break;
 	}
 	if (init < 0) {
+		connection_failed = 1;
 		return; // Network init failed
 	}
 
 	// DEBUG: Connect to me
 	socket = net_socket(PF_INET, SOCK_STREAM, 0);
 	if (socket < 0) {
+		connection_failed = 1;
 		return;
 	}
-	
+
 	struct sockaddr_in address;
 	memset(&address, 0, sizeof(address));
 
@@ -46,25 +50,27 @@ static void InitializeNetwork(const char *ip_str, const int port)
 	if (ret == 0) {
 		net_close(socket);
 		socket = -1;
+		connection_failed = 1;
 		return;
 	}
 	if (net_connect(socket, (struct sockaddr*)&address, sizeof(address)) < 0)
 	{
 		net_close(socket);
 		socket = -1;
+		connection_failed = 1;
 		return;
 	}
 }
 
 static ssize_t DebugPrint(struct _reent *r, void *fd, const char *ptr, size_t len)
 {
-	File_Log(ptr, len);
 	if (socket >= 0) {
 		size_t total_sent = 0;
 		while (total_sent < len) {
 			ssize_t res = net_send(socket, ptr + total_sent, len - total_sent, 0);
 			if (res < 0) {
-				break; // Connection error
+				socket = -1; // Connection lost
+				break;
 			}
 			if (res == 0) {
 				continue; // Retry
@@ -109,8 +115,15 @@ void Init_DebugConsole(const char *ip_str, int port)
 
 	_CPU_ISR_Disable(level);
 
-	if (ip_str)
+	if (ip_str) {
 		InitializeNetwork(ip_str, port);
+		// Print connection status BEFORE redirecting stdout
+		if (socket >= 0) {
+			printf("DEBUG: Network connected to %s:%d\n", ip_str, port);
+		} else {
+			printf("DEBUG: Network connection FAILED to %s:%d (check IP, run nc -l -p %d)\n", ip_str, port, port);
+		}
+	}
 
 	devoptab_list[STD_OUT] = (const devoptab_t *)&dotab_netout;
 	devoptab_list[STD_ERR] = (const devoptab_t *)&dotab_netout;
@@ -126,4 +139,9 @@ void Init_DebugConsole(const char *ip_str, int port)
 void Init_DebugConsole_Shutdown() {
 	net_deinit();
 	socket = -1;
+	connection_failed = 0;
+}
+
+int DebugConsole_GetStatus() {
+	return socket >= 0 ? 1 : (connection_failed ? -1 : 0);
 }
