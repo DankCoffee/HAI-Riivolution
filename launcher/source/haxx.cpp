40d7e49e907f5056ae39436e1f87bcf464b0ff5d
Dank Coffee <77390086+DankCoffee@users.noreply.github.com>
2026-03-26 11:54:32 +0400

    [LAUNCHER/DEBUG] Restore full reload with debug

--- launcher/source/haxx.cpp
+++ launcher/source/haxx.cpp
@@ -1700,10 +1700,19 @@ static bool do_exploit()
 
 		if (!patch_failed)
 		{
-			// Skip kernel reload - just use base exploit for module loading
-			printf("Using base exploit only\n");
+			printf("Reloading to patched IOS...\n");
+			shutdown_for_reload();
+			load_patched_ios(es_fd, new_ios, ios_rev + 1);
 			free(new_ios);
 			es_fd = 0;
+			printf("Waiting for reload...\n");
+			recover_from_reload((u32)HAXX_IOS);
+			printf("Reload done: IOS %d (rev %d)\n", IOS_GetVersion(), IOS_GetRevision());
+			if (IOS_GetVersion() != (u32)HAXX_IOS || IOS_GetRevision() != ios_rev+1) {
+				printf("Reload FAILED\n");
+				patch_failed = 1;
+			} else
+				printf("Reload OK\n");
 		}
 
 		if (!patch_failed)
